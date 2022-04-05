b8eb337eb76e0a5341df3051613a27f13954a9c7
Tim Pilius <tpilius@gmail.com>
2022-04-05 15:23:51 -0400

    Making the downloader class queue files for download
    Former-commit-id: a22deb9bd6e477bd0875a57e2724a2b1ca4dde85

--- BuildBackup/DataAccess/Downloader.cs
+++ BuildBackup/DataAccess/Downloader.cs
@@ -24,55 +24,25 @@ namespace BuildBackup.DataAccess
             _console = console;
         }
 
-        /// <summary>
-        /// Downloads all of the currently listed "archive" files from Battle.Net's CDN.  Each archive file is 256mb.
-        ///
-        /// More details on archive files : https://wowdev.wiki/TACT#Archives
-        /// </summary>
-        public void DownloadFullArchives(CDNConfigFile cdnConfig)
-        {
-            Console.WriteLine("Downloading full archive files....");
-
-            var progressBar = new ProgressBar(_console, PbStyle.SingleLine, cdnConfig.archives.Length);
-            int count = 0;
-            var timer = Stopwatch.StartNew();
-           
-            Parallel.ForEach(cdnConfig.archives, new ParallelOptions { MaxDegreeOfParallelism = 10 }, (entry) =>
-            {
-                _cdn.Get($"{_cdns.entries[0].path}/data/", entry, writeToDevNull: true);
-                progressBar.Refresh(count, $"     {_cdns.entries[0].path}/data/{entry}");
-                count++;
-            });
-            timer.Stop();
-            progressBar.Refresh(count, $"     Done! {Colors.Yellow(timer.Elapsed.ToString(@"mm\:ss\.FFFF"))}");
-        }
-
         //TODO comment
         public void DownloadUnarchivedFiles(CDNConfigFile cdnConfig, EncodingTable encodingTable)
         {
-            // Actually downloading the files
-            Console.WriteLine("Processing unarchived files");
-            var archiveIndexDictionary = IndexParser.BuildArchiveIndexes(_cdns.entries[0].path, cdnConfig, _cdn);
+            Console.Write("Processing individual, unarchived files ... ");
 
+            var timer = Stopwatch.StartNew();
+            var archiveIndexDictionary = IndexParser.BuildArchiveIndexes(_cdns.entries[0].path, cdnConfig, _cdn);
             foreach (var indexEntry in archiveIndexDictionary)
             {
                 encodingTable.EncodingDictionary.Remove(indexEntry.Key.ToUpper());
             }
-
-            Console.WriteLine($"     Downloading {Colors.Cyan(encodingTable.EncodingDictionary.Count())} unarchived files..");
-
-            int count = 0;
-            var timer = Stopwatch.StartNew();
-            var progressBar = new ProgressBar(_console, PbStyle.SingleLine, encodingTable.EncodingDictionary.Count, 50);
-            Parallel.ForEach(encodingTable.EncodingDictionary, new ParallelOptions { MaxDegreeOfParallelism = 20 }, entry =>
+            
+            foreach(var entry in encodingTable.EncodingDictionary)
             {
-                _cdn.Get($"{_cdns.entries[0].path}/data/", entry.Key, writeToDevNull: true);
-                //progressBar.Refresh(count, $"     {_cdns.entries[0].path}/data/{entry}");
-                count++;
-            });
+                _cdn.QueueRequest($"{_cdns.entries[0].path}/data/", entry.Key, writeToDevNull: true);
+            }
 
             timer.Stop();
-            progressBar.Refresh(count, $"     Done! {Colors.Yellow(timer.Elapsed.ToString(@"mm\:ss\.FFFF"))}");
+            Console.WriteLine($"{Colors.Yellow(timer.Elapsed.ToString(@"mm\:ss\.FFFF"))}");
         }
 
         public void DownloadUnarchivedIndexFiles(CDNConfigFile cdnConfig)
