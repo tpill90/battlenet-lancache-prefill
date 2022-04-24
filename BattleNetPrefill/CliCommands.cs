2d7907bcf85c5589096f4091303489e533da1c86
Tim Pilius <tpilius@gmail.com>
2022-04-24 15:46:58 -0400

    Cli documentation

--- BattleNetPrefill/CliCommands.cs
+++ BattleNetPrefill/CliCommands.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Numerics;
 using System.Threading.Tasks;
 using BattleNetPrefill.Utils;
 using CliFx;
 using CliFx.Attributes;
+using CliFx.Exceptions;
 using CliFx.Extensibility;
 using CliFx.Infrastructure;
 using Spectre.Console;
@@ -39,19 +38,36 @@ namespace BattleNetPrefill
             }
         }
 
-        [Command("prefill", Description = "Downloads the latest files for a specified product(s)")]
+        [Command("prefill", Description = "Downloads the latest files for one or more specified product(s)")]
         public class PrefillCommand : ICommand
         {
-            [CommandOption("products", IsRequired = true, Converter = typeof(TactProductConverter))]
+            [CommandOption("products", shortName: 'p', Description = "Specifies which products to prefill.  Example '--products s1' will prefill Starcraft 1", Converter = typeof(TactProductConverter))]
             public IReadOnlyList<TactProduct> ProductCodes { get; init; }
 
-            //TODO comment
-            [CommandOption("no-local-cache")]
+            [CommandOption("all", Description = "Prefills all available products.  Includes all Activision and Blizzard games")]
+            public bool PrefillAllProducts { get; init; }
+
+            [CommandOption("activision", Description = "Prefills all Activision products.")]
+            public bool PrefillActivision { get; init; }
+
+            [CommandOption("blizzard", Description = "Prefills all Blizzard products.")]
+            public bool PrefillBlizzard { get; init; }
+
+            [CommandOption("nocache", Description = "Skips using locally cached files.  Saves disk space, at the expense of slower subsequent runs.")]
             public bool NoLocalCache { get; init; }
 
             public ValueTask ExecuteAsync(IConsole console)
             {
-                foreach (var code in ProductCodes.Distinct().ToList())
+                List<TactProduct> productsToProcess = BuildProductListFromArgs();
+
+                if (productsToProcess.Count == 0)
+                {
+                    throw new CommandException("At least one product is required!  Use '--products' to specify which products to load, " +
+                                               "or use bulk flags '--all', '--activision', or '--blizzard' to load predefined groups", 1, true);
+                }
+
+                AnsiConsole.WriteLine($"Prefilling {Colors.Yellow(productsToProcess.Count)} products");
+                foreach (var code in productsToProcess.Distinct().ToList())
                 {
                     TactProductHandler.ProcessProduct(code, AnsiConsole.Create(new AnsiConsoleSettings()),
                         Config.UseCdnDebugMode, Config.WriteOutputFiles, Config.ShowDebugStats, NoLocalCache);
@@ -60,26 +76,28 @@ namespace BattleNetPrefill
                 return default;
             }
 
-        }
-
-        //TODO comment
-        [Command("prefill-all")]
-        public class PrefillAllCommand : ICommand
-        {
-            //TODO comment
-            [CommandOption("no-local-cache")]
-            public bool NoLocalCache { get; init; }
-
-            public ValueTask ExecuteAsync(IConsole console)
+            private List<TactProduct> BuildProductListFromArgs()
             {
-                foreach (var code in TactProduct.AllEnumValues)
+                var productsToProcess = new List<TactProduct>();
+                if (ProductCodes != null)
                 {
-                    TactProductHandler.ProcessProduct(code, AnsiConsole.Create(new AnsiConsoleSettings()),
-                        Config.UseCdnDebugMode, Config.WriteOutputFiles, Config.ShowDebugStats, NoLocalCache);
+                    productsToProcess.AddRange(ProductCodes);
                 }
-                return default;
-            }
+                if (PrefillAllProducts)
+                {
+                    productsToProcess.AddRange(TactProduct.AllEnumValues);
+                }
+                if (PrefillActivision)
+                {
+                    productsToProcess.AddRange(TactProduct.AllEnumValues.Where(e => e.IsActivision));
+                }
+                if (PrefillBlizzard)
+                {
+                    productsToProcess.AddRange(TactProduct.AllEnumValues.Where(e => e.IsBlizzard));
+                }
 
+                return productsToProcess;
+            }
         }
 
         public class TactProductConverter : BindingConverter<TactProduct>
