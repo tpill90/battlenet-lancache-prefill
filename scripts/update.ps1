6103973f255ff2c82229d3e49e6da9bf0d77ed6d
Tim Pilius <tpilius@gmail.com>
2023-05-09 17:24:26 -0400

    Adding auto update script

---
+++ scripts/update.ps1
@@ -0,0 +1,40 @@
+Set-Location $PSScriptRoot
+
+# Getting latest version tag
+Write-Host -ForegroundColor Yellow "Checking for latest version"
+$versionApi = "https://api.github.com/repos/tpill90/battlenet-lancache-prefill/releases"
+$versions = Invoke-RestMethod -Uri $versionApi
+
+# Finding latest asset
+$windowsAsset = $versions[0].assets | Where-Object { $_.name.Contains("win-x64")}
+$latestVersion = $versions[0].name
+Write-Host "Found latest version : " -NoNewline
+Write-Host -ForegroundColor Cyan $latestVersion
+
+# Seeing if BattlenetPrefill is already installed and up to date
+if(Test-Path "BattlenetPrefill.exe")
+{
+    $currentVersion = (.\BattlenetPrefill.exe --version)
+    $upToDate = $currentVersion -eq $latestVersion
+
+    if($upToDate)
+    {
+        Write-Host "Already up to date !" -ForegroundColor Yellow
+        return
+    }
+}
+
+# Downloading
+Write-Host -ForegroundColor Yellow "Downloading..."
+Invoke-WebRequest $windowsAsset.browser_download_url -OutFile $windowsAsset.name
+
+# Unzipping
+Write-Host -ForegroundColor Yellow "Unzipping..."
+Expand-Archive -Force $windowsAsset.name -DestinationPath .
+Copy-Item "$($windowsAsset.name.Replace('.zip', ''))\BattlenetPrefill.exe"
+
+# Cleanup 
+Remove-Item $windowsAsset.name
+Remove-Item -Force -Recurse "$($windowsAsset.name.Replace('.zip', ''))"
+
+Write-Host -ForegroundColor Cyan "Complete !"
---
+++ scripts/update.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+Cyan='\033[0;36m'
+RED='\033[0;31m'
+Yellow='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Checking for required software
+if ! [ -x "$(command -v curl)" ]; then
+  echo -e "${RED}Required software curl is not installed.${NC}" >&2
+  exit 1
+fi
+if ! [ -x "$(command -v jq)" ]; then
+  echo -e "${RED}Required software jq is not installed.${NC}" >&2
+  exit 1
+fi
+if ! [ -x "$(command -v unzip)" ]; then
+  echo -e "${RED}Required software unzip is not installed.${NC}" >&2
+  exit 1
+fi
+if ! [ -x "$(command -v wget)" ]; then
+  echo -e "${RED}Required software wget is not installed.${NC}" >&2
+  exit 1
+fi
+
+# Getting latest version tag
+echo -e "${Yellow} Checking for latest version ${NC}"
+VERSION_API="https://api.github.com/repos/tpill90/battlenet-lancache-prefill/releases"
+LATEST_TAG=$(curl -s $VERSION_API | jq -r '.[0].tag_name' | cut -c 2-)
+
+if [ -z "${LATEST_TAG}" ]; then
+    echo -e " ${RED}Something went wrong, unable to get latest version!${NC}"
+    exit 1
+fi
+echo -e " Found latest version : ${Cyan} ${LATEST_TAG} ${NC}"
+
+# Checking to see if BattlenetPrefill is already up to date
+if [ -f /BattlenetPrefill ]; then
+    CURRENT_VERSION=$(./BattlenetPrefill --version)
+
+    if [ "${CURRENT_VERSION}" == "v${LATEST_TAG}" ]; then
+        echo -e "${Yellow} Already up to date !${NC}"
+        exit
+    fi
+fi
+
+# Downloading latest version
+echo -e "${Yellow} Downloading... ${NC}"
+DOWNLOAD_URL="https://github.com/tpill90/battlenet-lancache-prefill/releases/download/v${LATEST_TAG}/BattlenetPrefill-${LATEST_TAG}-linux-x64.zip"
+wget -q -nc --show-progress --progress=bar:force:noscroll $DOWNLOAD_URL
+
+# Unzip
+echo -e "${Yellow} Unzipping... ${NC}"
+unzip -q -j -o BattlenetPrefill-${LATEST_TAG}-linux-x64.zip
+
+# Required so executable permissions don't get overwritten by unzip
+chmod +x BattlenetPrefill update.sh
+
+# Cleanup
+rm BattlenetPrefill-${LATEST_TAG}-linux-x64.zip
+
+echo -e " ${Cyan} Complete! ${NC}"
